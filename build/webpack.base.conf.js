0c85dffd6e58c62c4f806f5d0eaa9f156e58cefc
Patrick Davey <patrick.davey@gmail.com>
2017-06-30 16:15:23 +1200

    fix up the import

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,6 +22,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
+      'vue-strap': path.resolve(__dirname, '../node_modules/vue-strap/dist/vue-strap'),
       '@': resolve('src')
     }
   },
